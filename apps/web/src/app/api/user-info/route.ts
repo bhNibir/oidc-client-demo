81ac8efdd0f2521d897b29da27b85f6d37d43385
Biplob Hasan Nibir <biplob.asanibir@gmail.com>
2025-09-16 12:34:01 +0600

    Refactor user info API route to simplify header handling; directly pass headers to session retrieval.

--- apps/web/src/app/api/user-info/route.ts
+++ apps/web/src/app/api/user-info/route.ts
@@ -8,21 +8,12 @@ import { headers } from "next/headers";
 
 
 export async function GET(request: NextRequest) {
-  // Get headers as a plain object
-  const headersObj: Record<string, string> = {};
-  request.headers.forEach((value, key) => {
-    headersObj[key] = value;
-  });
 
-
-  const headerList = await headers();
-  console.log("headerList", headerList);
+  
   const session = await auth.api.getSession({
-    headers: headerList // you need to pass the headers object.
-})
-
+    headers: await headers() // some endpoints might require headers
+  })
   return NextResponse.json({
-    session: session,
-    headers: headersObj,
+    session: session
   });
 }
